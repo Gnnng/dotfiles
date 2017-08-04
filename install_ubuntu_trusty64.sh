9794c0fa063371a4ca7696f6e544a54f9b80b69f
Gong Deli <gnnnnng@gmail.com>
2017-08-04 20:16:54 +0800

    adopt the installing changes brought by Linuxbrew

--- install_ubuntu_trusty64.sh
+++ install_ubuntu_trusty64.sh
@@ -10,10 +10,10 @@ sudo apt-get update && sudo apt-get install -y $packages
 
 # install linuxbrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.zshrc_local
-echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> ~/.zshrc_local
-echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> ~/.zshrc_local
-export PATH="$HOME/.linuxbrew/bin:$PATH"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.zshrc_local
+echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >> ~/.zshrc_local
+echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >> ~/.zshrc_local
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
 ################# For Vim 8.0+
 brew install vim
