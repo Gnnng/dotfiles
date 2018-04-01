dce822c81c50e4246b9cf7317ec9e306eb260633
Gong Deli <gnnnnng@gmail.com>
2018-04-01 23:46:41 +0800

    fix

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/stable
+echo "deb http://ppa.launchpad.net/neovim-ppa/stable/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/neovim.list
+echo "deb-src http://ppa.launchpad.net/neovim-ppa/stable/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/neovim.list
+
 sudo apt-get update
 sudo apt-get install -y neovim
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip
