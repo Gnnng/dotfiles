363b789e40db817337fdbeff8803f2ee45245e17
Gong Deli <gnnnnng@gmail.com>
2018-04-01 17:48:05 +0800

    add neovim install

---
+++ install_neovim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+pip install neovim
+pip3 install neovim
+
+
+
