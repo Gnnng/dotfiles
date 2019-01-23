8c41bfc4840b437ef94f902953353e8cbc45dbf4
Gong Deli <gnnnnng@gmail.com>
2019-01-23 11:28:50 +0800

    fish: fix aws-auth

--- fish/.config/fish/functions/aws-auth.fish
+++ fish/.config/fish/functions/aws-auth.fish
@@ -5,6 +5,9 @@ function aws-auth
         echo "Usage: $0 TOKEN"
         return 1
     end
+    set -e AWS_SESSION_TOKEN
+    set -e AWS_ACCESS_KEY_ID
+    set -e AWS_SECRET_ACCESS_KEY
     set arn (aws sts get-caller-identity --output text --query 'Arn' | sed 's/user/mfa/g'); or return $status
     set result (aws sts get-session-token --serial-number $arn --token-code $token); or return $status
     set -gx AWS_ACCESS_KEY_ID (echo $result | jq -r '.Credentials.AccessKeyId')
